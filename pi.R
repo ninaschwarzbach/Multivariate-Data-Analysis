d9462d0d786da7a02112a3e28dcf4842c87b0ebe
ninaschwarzbach <n.r.schwarzbach@rug.nl>
2024-04-03 16:15:58 +0900

    Yet a 3rd way to estimate pi

--- pi.R
+++ pi.R
@@ -8,8 +8,23 @@ mean(x^2 + y^2 < 1) * 4
 
 # We can also try using the formula of the volume of a sphere.
 # For radius 1, the volume is (4/3)*pi.
-# So the following should also work to approx. pi:
+# So the following should also work to approx. pi (why??):
 x     <- runif(Nreps, -1, 1)
 y     <- runif(Nreps, -1, 1)
 z     <- runif(Nreps, -1, 1)
 mean(x^2 + y^2 + z^2 < 1) * 6
+
+# Humm, would this also estimate pi?
+vec <- rep(NA, Nreps)
+for (i in 1:Nreps)
+{
+  tmp <- 0
+  ind <- 0
+  while (tmp <= 1) 
+  {
+    tmp <- tmp + runif(1, 0, 1)
+    ind <- ind + 1
+  }
+  vec[i] <- ind
+}
+mean(vec)
